fe2542ce408d1ec69ec511736170b37a54e7dc53
cyberroadie <cyberroadie@gmail.com>
2009-07-29 08:24:06 +0100

    Added name equality test exercises

---
+++ junit-examples/src/main/java/net/transformtorhuis/junit/agilejava/Name.java
@@ -0,0 +1,42 @@
+package net.transformtorhuis.junit.agilejava;
+
+/**
+ *
+ * @author cyberroadie
+ */
+public class Name {
+
+    private String name = null;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object ==  null) {
+            return false;
+        }
+        if(!(object instanceof Name)) {
+            return false;
+        }
+        Name testName = (Name) object;
+        return this.name.equals(testName.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+}
