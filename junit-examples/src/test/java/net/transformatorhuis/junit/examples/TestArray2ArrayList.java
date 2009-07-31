a37cc73c2f5283c742f59e04b16c13ae3e296c95
cyberroadie <cyberroadie@gmail.com>
2009-07-31 19:08:16 +0100

    Fixed test, forgot to split string

--- junit-examples/src/test/java/net/transformatorhuis/junit/examples/TestArray2ArrayList.java
+++ junit-examples/src/test/java/net/transformatorhuis/junit/examples/TestArray2ArrayList.java
@@ -15,11 +15,12 @@ public class TestArray2ArrayList {
     @Test
     public void testArray() {
         String blah = "hello world lala";
-        List arrayStringList = Arrays.asList(blah);
+        List arrayStringList = Arrays.asList(blah.split(" "));
         try {
             // This won't work because this list is a view on an array
             // which immutable
             arrayStringList.remove(2);
+            fail("Should throw UnsupportedOperationException");
         } catch (Exception ex) {
             assertTrue(ex instanceof UnsupportedOperationException);
         }
