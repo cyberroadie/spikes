86744d90b5c2b126131100287de50007081dbc3f
cyberroadie <cyberroadie@gmail.com>
2009-08-02 08:55:03 +0100

    Cleanup package structure

---
+++ junit-examples/src/test/java/net/transformatorhuis/junit/agilejava/WordCountTest.java
@@ -0,0 +1,28 @@
+package net.transformatorhuis.junit.agilejava;
+
+import net.transformtorhuis.junit.agilejava.*;
+import java.util.Map;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cyberroadie
+ */
+public class WordCountTest {
+
+    public WordCountTest() {
+    }
+
+    /**
+     * Test of wordCount method, of class WordCount.
+     */
+    @Test
+    public void testWordCount() {
+        WordCount wordCount = new WordCount();
+        Map words = wordCount.getWordCount();
+        assertEquals(1, words.get("string"));
+        assertEquals(3, words.get("the"));
+    }
+
+}
