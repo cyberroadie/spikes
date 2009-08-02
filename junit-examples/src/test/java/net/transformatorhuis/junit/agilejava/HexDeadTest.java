ecd608a9acf3646cab1ae9560810f5830567919d
cyberroadie <cyberroadie@gmail.com>
2009-08-02 14:32:29 +0100

    Added 0xDEAD test

---
+++ junit-examples/src/test/java/net/transformatorhuis/junit/agilejava/HexDeadTest.java
@@ -0,0 +1,16 @@
+package net.transformatorhuis.junit.agilejava;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author cyberroadie
+ */
+public class HexDeadTest {
+
+    @Test
+    public void testDead() {
+        int x = 0xDEAD;
+        System.out.print("0xDEAD: " + x);
+    }
+}
