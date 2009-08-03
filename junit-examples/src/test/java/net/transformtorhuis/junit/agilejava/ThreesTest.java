6ea98416b7b63ab872b8533c371757a3116d1ba8
Olivier Van Acker <cyberroadie@bugslap.(none)>
2009-08-03 08:17:28 +0100

    Added modulus exercise

---
+++ junit-examples/src/test/java/net/transformtorhuis/junit/agilejava/ThreesTest.java
@@ -0,0 +1,39 @@
+package net.transformtorhuis.junit.agilejava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cyberroadie
+ */
+public class ThreesTest {
+
+    @Test
+    public void testGetThrees() {
+        System.out.println("getThrees");
+        ArrayList expResult = new ArrayList();
+        expResult.addAll(Arrays.asList(3, 6, 9));
+        int[] values = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        ArrayList result = Threes.getThrees(values);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetThreesWithouModulus() {
+        System.out.println("getThrees");
+        ArrayList expResult = new ArrayList();
+        expResult.addAll(Arrays.asList(3, 6, 9));
+        int[] values = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        ArrayList result = Threes.getThreesWithoutMod(values);
+        assertEquals(expResult, result);
+    }
+
+}
